3229f016132af06cc2d51a80496a07b7361988ff
Daniel Eder <daniel@deder.at>
2017-07-20 14:13:46 +0200

    fix invalid memset... it is not necessary

--- memtable.c
+++ memtable.c
@@ -22,9 +22,9 @@ void init_memory_table(size_t blkSize)
     if(memory_table_len < base) {
         // enlarge list
         memory_table = (memory_page**) realloc(memory_table, base*sizeof(memory_page*));
-        memset(memory_table+(memory_table_len+1), 
+        /*memset(memory_table+(memory_table_len+1), 
                0, 
-               (base-memory_table_len)*sizeof(memory_page*));
+               (base-memory_table_len)*sizeof(memory_page*));*/
     }
 
     printf("initialisng first pages...\n");
